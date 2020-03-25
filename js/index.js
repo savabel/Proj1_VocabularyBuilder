ec2cc683d14f8eb6f18d75b810aa47b06676a377
Christopher Bourque <c@bourque.xyz>
2020-03-24 20:49:49 -0400

    Updated index file with comments and added DOM elements to js file

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// DOM elements
+
+let wordSearchedInput = document.getElementById("wordSearchedInput");  // To get word entered by user
+let wordSearchedButton = document.getElementById("wordSearchedButton");  // To get button click using addEventListener
+let wordDefinitionList = document.getElementById("wordDefinitionList");  // To insert definitions in UI
+let wordSynonymList = document.getElementById("wordSynonymList");  // To insert synonyms in UI
+let wordAntonymList = document.getElementById("wordAntonymList");  // To insert antonyms in UI
+let associatedWordsDiagram = document.getElementById("associatedWordsDiagram");  // To insert associated words bubble diagram
+ 
